500130a5bf50fad404a8ae01dc0a5659ba17c5e1
Hasymi Al <68429761+zxyvyx@users.noreply.github.com>
2022-12-28 12:59:47 +0700

    feat: implementation of api search users and get repositories by username
    Signed-off-by: Hasymi Al <68429761+zxyvyx@users.noreply.github.com>

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,102 @@
 import Head from 'next/head';
+import { useContext } from 'react';
+import SearchBox from '../components/SearchBox';
+import ActiveUserContext from '../contexts/ActiveUserContext';
+import LoaderContext from '../contexts/LoaderContext';
+import SearchResultContext from '../contexts/SearchResultContext';
 
 export default function Home() {
+  const { onLoading, setOnLoading } = useContext(LoaderContext);
+  const { setSearchResult } = useContext(SearchResultContext);
+  const { setSelectedUser } = useContext(ActiveUserContext);
+
+  const getUserLists = async (value) => {
+    setOnLoading({ ...onLoading, searchUser: true });
+    const endpoint = `/search/users?${new URLSearchParams(value)}`;
+    await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.github+json',
+      },
+    })
+      .then(async (response) => {
+        if (response.status === 200) return Promise.resolve(response.json());
+        const responseInJson = await Promise.resolve(response.json());
+        return Promise.reject(responseInJson);
+      })
+      .then(
+        (result) => {
+          const temp = result.items.map((item) => ({
+            id: item.id,
+            name: item.login,
+            image: item.avatar_url,
+          }));
+          setSearchResult(temp);
+          console.log(temp);
+        },
+        (error) => {
+          console.log('Error: ', error);
+          setSearchResult([]);
+        }
+      )
+      .catch((catchError) => {
+        console.log('Catch: ', catchError);
+        setSearchResult([]);
+      })
+      .finally(() => {
+        setOnLoading({ ...onLoading, searchUser: false });
+        console.log('end fetch');
+      });
+  };
+
+  const getRepositoriesByUser = async (username, id) => {
+    setOnLoading({ ...onLoading, listRepository: true });
+    const endpoint = `/users/${username}/repos`;
+    await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.github+json',
+      },
+    })
+      .then(async (response) => {
+        if (response.status === 200) return Promise.resolve(response.json());
+        const responseInJson = await Promise.resolve(response.json());
+        return Promise.reject(responseInJson);
+      })
+      .then(
+        (result) => {
+          const temp = result.map((item) => ({
+            id: item.id,
+            link: item.html_url,
+            name: item.name,
+            stars: item.stargazers_count,
+            language: item.language,
+            dateModified: item.updated_at,
+          }));
+          const tempUser = {
+            name: username,
+            id,
+            repo: temp,
+          };
+          setSelectedUser(tempUser);
+        },
+        (error) => {
+          console.log('Error: ', error);
+          setSearchResult(null);
+        }
+      )
+      .catch((catchError) => {
+        console.log('Catch: ', catchError);
+        setSearchResult(null);
+      })
+      .finally(() => {
+        setOnLoading({ ...onLoading, listRepository: false });
+        console.log('end fetch');
+      });
+  };
+
   return (
     <>
       <Head>
@@ -12,10 +108,14 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <div className='text-2xl font-bold text-teal-500'>
+      <main className='mx-auto max-w-4xl px-2 sm:px-6 md:px-10'>
+        <div className='font-inter text-2xl font-bold text-teal-500'>
           Interview React JS - Next JS
         </div>
+        <SearchBox
+          onSubmit={getUserLists}
+          onSelectUser={getRepositoriesByUser}
+        />
       </main>
     </>
   );
